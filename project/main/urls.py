3b7e0e6f121e11faff477ec8d513f43e853b1c56
Qobiljon <madrahimovq@gmail.com>
2026-08-18 15:25:37 +0500

    6-oy 8-dars tugadi

--- project/main/urls.py
+++ project/main/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import index, detail, books_by_category, save_comment
+from .views import (index, detail, books_by_category, save_comment, update_comment,
+                    delete_comment)
 
 urlpatterns = [
     path('', index, name='home'),
     path('category/<int:category_id>/', books_by_category, name='books_by_category'),
     path('book/<int:book_id>/', detail, name='detail'),
     path('add/comment/<int:book_id>/', save_comment, name='save_comment'),
+    path('update/comment/<int:comment_id>/', update_comment, name='update_comment'),
+    path('delete/comment/<int:comment_id>/', delete_comment, name='delete_comment'),
 ]
--- project/main/views.py
+++ project/main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest
+from pip._internal.utils import retry
 
 from .models import Category, Book, Comment
+from .forms import CommentForm
 
 
 def index(request):
@@ -34,9 +36,13 @@ def detail(request: HttpRequest, book_id):
 def save_comment(request: HttpRequest, book_id):
     if request.user.is_authenticated:
         if request.method == 'POST':
-            text = request.POST.get('text')
-            book = Book.objects.get(id=book_id)
-            comment = Comment.objects.create(text=text, book=book, user=request.user)
+            # text = request.POST.get('text')
+            form = CommentForm(data=request.POST)
+            if form.is_valid():
+                book = Book.objects.get(id=book_id)
+                comment = Comment.objects.create(text=form.cleaned_data.get("text"), book=book, user=request.user)
+            else:
+                print("simvollar soni 500 tadan ko'p")
             return redirect('detail', book_id=book_id)
         else:
             return redirect('home')
@@ -45,7 +51,36 @@ def save_comment(request: HttpRequest, book_id):
         return redirect('home')
 
 
+def update_comment(request, comment_id):
+    comment = Comment.objects.get(id=comment_id)
+    if request.user.is_authenticated and request.user == comment.user:
+        if request.method == 'POST':
+            form = CommentForm(data=request.POST)
+            if form.is_valid():
+                comment.text = form.cleaned_data.get("text")
+                comment.save()
+                return redirect("detail", book_id=comment.book.id)
+        else:
+            form = CommentForm(initial={"text": comment.text})
+        context = {
+            "form": form
+        }
+        return render(request, "main/comment_update.html", context)
+    else:
+        print('login qiling')
+        return redirect('home')
 
 
-
+def delete_comment(request, comment_id):
+    comment = Comment.objects.get(id=comment_id)
+    if request.user.is_authenticated and request.user == comment.user or request.user.is_superuser:
+        book_id = comment.book.id
+        if request.method == 'POST':
+            comment.delete()
+            return redirect('detail', book_id=book_id)
+        else:
+            return render(request, "main/confirm_delete.html", {"comment": comment})
+    else:
+        print('login qiling')
+        return redirect('home')
 
